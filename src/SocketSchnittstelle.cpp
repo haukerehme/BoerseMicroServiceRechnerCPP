1ecd440b79d1d7c8f45abcdfdce17c450d44628d
haukerehme <hauke.rehme-schlueter@hotmail.com>
2016-11-06 10:56:15 +0100

    init

---
+++ src/SocketSchnittstelle.cpp
@@ -0,0 +1,18 @@
+/*
+ * SocketSchnittstelle.cpp
+ *
+ *  Created on: 06.11.2016
+ *      Author: hrs
+ */
+
+#include "SocketSchnittstelle.h"
+
+SocketSchnittstelle::SocketSchnittstelle() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SocketSchnittstelle::~SocketSchnittstelle() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/SocketSchnittstelle.h
@@ -0,0 +1,17 @@
+/*
+ * SocketSchnittstelle.h
+ *
+ *  Created on: 06.11.2016
+ *      Author: hrs
+ */
+
+#ifndef SOCKETSCHNITTSTELLE_H_
+#define SOCKETSCHNITTSTELLE_H_
+
+class SocketSchnittstelle {
+public:
+	SocketSchnittstelle();
+	virtual ~SocketSchnittstelle();
+};
+
+#endif /* SOCKETSCHNITTSTELLE_H_ */
